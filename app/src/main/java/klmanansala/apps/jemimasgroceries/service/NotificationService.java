4f9293d15d55158ced2e1a3afeaed565437e37e8
Kevin Manansala <klmanansala@gmail.com>
2015-07-09 14:56:56 +0800

    tested notif code

--- app/src/main/java/klmanansala/apps/jemimasgroceries/service/NotificationService.java
+++ app/src/main/java/klmanansala/apps/jemimasgroceries/service/NotificationService.java
@@ -39,52 +39,7 @@ public class NotificationService extends IntentService {
 
     @Override
     protected void onHandleIntent(Intent intent) {
-        Calendar cal = Calendar.getInstance();
-        long dateToday = cal.getTimeInMillis();
-
-        Uri queryUri = GroceriesContract.InventoryEntry.buildInventoryWithDateUri(dateToday);
-        Cursor cursor = this.getContentResolver().query(queryUri, COLUMNS, null, null, null);
-
-        StringBuilder contentText = new StringBuilder();
-        contentText.append(this.getString(R.string.expiring_items));
-
-        if(cursor.moveToFirst()){
-            do {
-                String itemName = cursor.getString(COL_NAME);
-                contentText.append(" " + itemName + ",");
-            } while(cursor.moveToNext());
-        }
-
-        String title = this.getString(R.string.app_name);
-        int iconId = R.mipmap.ic_launcher;
-
-        NotificationCompat.Builder notifBuilder = new NotificationCompat.Builder(this);
-        notifBuilder.setContentText(contentText);
-        notifBuilder.setContentTitle(title);
-        notifBuilder.setSmallIcon(iconId);
-
-        Intent notifIntent = new Intent(this, MainActivity.class);
-
-        // The stack builder object will contain an artificial back stack for the
-        // started Activity.
-        // This ensures that navigating backward from the Activity leads out of
-        // your application to the Home screen.
-        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
-        // Adds the back stack for the Intent (but not the Intent itself)
-        stackBuilder.addParentStack(MainActivity.class);
-        // Adds the Intent that starts the Activity to the top of the stack
-        stackBuilder.addNextIntent(notifIntent);
-        PendingIntent pendingIntent =
-                stackBuilder.getPendingIntent(
-                        0,
-                        PendingIntent.FLAG_UPDATE_CURRENT
-                );
-        notifBuilder.setContentIntent(pendingIntent);
-
-        Notification notif = notifBuilder.build();
-        NotificationManager notificationManager =
-                (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationManager.notify(NOTIFICATION_ID, notif);
+        notify(this);
     }
 
     public static class AlarmReceiver extends BroadcastReceiver {
@@ -93,5 +48,48 @@ public class NotificationService extends IntentService {
             Intent notifIntent = new Intent(context, NotificationService.class);
             context.startService(notifIntent);
         }
+    }
+
+    public static void notify(Context context){
+        Calendar cal = Calendar.getInstance();
+        long dateToday = cal.getTimeInMillis();
+
+        Uri queryUri = GroceriesContract.InventoryEntry.buildInventoryWithDateUri(dateToday);
+        Cursor cursor = context.getContentResolver().query(queryUri, COLUMNS, null, null, null);
+
+        if(cursor.moveToFirst()){
+            String contentText = context.getString(R.string.expiring_items);
+            String title = context.getString(R.string.app_name);
+            int iconId = R.mipmap.ic_launcher;
+
+            NotificationCompat.Builder notifBuilder = new NotificationCompat.Builder(context);
+            notifBuilder.setContentText(contentText);
+            notifBuilder.setContentTitle(title);
+            notifBuilder.setSmallIcon(iconId);
+
+            Intent notifIntent = new Intent(context, MainActivity.class);
+
+            // The stack builder object will contain an artificial back stack for the
+            // started Activity.
+            // This ensures that navigating backward from the Activity leads out of
+            // your application to the Home screen.
+            TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+            // Adds the back stack for the Intent (but not the Intent itself)
+            stackBuilder.addParentStack(MainActivity.class);
+            // Adds the Intent that starts the Activity to the top of the stack
+            stackBuilder.addNextIntent(notifIntent);
+            PendingIntent pendingIntent =
+                    stackBuilder.getPendingIntent(
+                            0,
+                            PendingIntent.FLAG_UPDATE_CURRENT
+                    );
+            notifBuilder.setContentIntent(pendingIntent);
+
+            Notification notif = notifBuilder.build();
+            NotificationManager notificationManager =
+                    (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            notificationManager.notify(NOTIFICATION_ID, notif);
+        }
+
     }
 }
